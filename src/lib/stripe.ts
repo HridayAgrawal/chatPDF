0001986b26ec6f67758168f694969152a2a9f0f5
Hriday Agrawal <hriday.agrawal03@gmail.com>
2024-05-14 18:38:38 +0530

    initial commit

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,7 @@ import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import Stripe from 'stripe'
 
+//stripe info
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2023-08-16',
   typescript: true,
