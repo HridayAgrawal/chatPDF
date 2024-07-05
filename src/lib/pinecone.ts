43b6f487ae98d5845940c2d67f2ddbe41917f4b0
Hriday Agrawal <hriday.agrawal03@gmail.com>
2024-07-05 13:17:35 +0530

    initialcommit

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -5,7 +5,7 @@ export const getPineconeClient = async () => {
 
   await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-east1-gcp-free',
+    environment: 'us-east-1',
     // environment: 'asia-southeast1-gcp-free'
   })
 
